b0459ea9bc1f71df9af2790d98cee287243ac63f
Kck432 <kck43@pitt.edu>
2025-04-21 11:46:28 -0400

    Updated evaluate with alignment

--- evaluate.py
+++ evaluate.py
@@ -1,5 +1,7 @@
 import re
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+import difflib
+
 
 def load(filename): #process input and output files
     with open(filename, 'r',encoding='utf-8') as file:
@@ -16,14 +18,10 @@ def load(filename): #process input and output files
     ind = 0
     for char in characters:
         if char == "@": #if @ - inside markers
-           # print(char== "@")
             inside_markers = True
         elif char =="#": #if # - outside markers
-            #print(char, "outside")
             inside_markers = False
         elif inside_markers: # if inside markers - reformat char
-           # print(char == "#")
-           # print(char, "inside")
             characters[ind] = "@@" + char + "##"
         else:
             pass
@@ -35,7 +33,50 @@ def load(filename): #process input and output files
         if char != "@" and char != "#" and char != "@@@##": #strip of remaining outside markers and mistakes
             result.append(char)
 
-    return (result)
+    return result
+
+
+def align_sequences(true, pred):
+    sequence = difflib.SequenceMatcher(None, true, pred)
+    #result of this:
+    #(tag, i1, i2, j1, j2)
+
+    #'replace' a[i1:i2] should be replaced by b[j1:j2].
+    #'delete' a[i1:i2] should be deleted. Note that j1 == j2 in this case.
+    #'insert' b[j1:j2] should be inserted at a[i1:i1]. Note that i1 == i2 in this case.
+    #'equal' a[i1:i2] == b[j1:j2] (the sub-sequences are equal).
+
+    #print(sm.get_opcodes())
+
+    aligned_true = []
+    aligned_pred = []
+
+    for opcode, i1, i2, j1, j2 in sequence.get_opcodes():
+        if opcode == 'equal': #add to both
+            #print("T", (true[i1]),pred[j1])
+            aligned_true.extend(true[i1:i2])
+            aligned_pred.extend(pred[j1:j2])
+
+        elif opcode == 'replace': #if difference in length of replace - add '-', else add chars to both
+            max_len = max(i2 - i1, j2 - j1)
+            ref_segment = list(true[i1:i2]) + ['-'] * (max_len - (i2 - i1))
+            pred_segment = list(pred[j1:j2]) + ['-'] * (max_len - (j2 - j1))
+            
+            aligned_true.extend(ref_segment)
+            # print("A:", aligned_true)
+            aligned_pred.extend(pred_segment)
+            # print("P:", aligned_pred)
+            
+        elif opcode == 'delete': #add as many '-'s as there are deleted characters
+            aligned_true.extend(true[i1:i2])
+            aligned_pred.extend(['-'] * (i2 - i1))
+
+        elif opcode == 'insert': #add as many '-'s as there are inserted characters
+            aligned_true.extend(['-'] * (j2 - j1))
+            aligned_pred.extend(pred[j1:j2])
+
+    return aligned_true, aligned_pred
+
 
 
 def calculate_metrics(truths, predictions):
@@ -52,18 +93,9 @@ if __name__ == "__main__":
 
     predictions = load(predictions) #process
     truths = load(truths)
+    predictions, truths = align_sequences(truths, predictions) #get alignments
 
-    ind = 0 #how far until first different char
-
-    for x in range(len(predictions)):
-        if(truths[x].strip("@#") == predictions[x].strip("@#")):
-            ind +=1
-        else:
-            break
-
-    #only get matching - will change this
-    predictions = predictions[:ind]
-    truths = truths[:ind]
-
-    evaluation_results = calculate_metrics(truths, predictions)
+    evaluation_results = calculate_metrics(predictions, truths) #get metrics
+    
     print("Accuracy: ", evaluation_results[0],"\nPrecision: ", evaluation_results[1], "\nRecall: ", evaluation_results[2], "\nF1 Score: ", evaluation_results[3])
+
